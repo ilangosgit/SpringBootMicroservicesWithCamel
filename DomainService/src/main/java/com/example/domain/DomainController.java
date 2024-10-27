0dd7cd705927d8f0b3550aaa722647eddcd5a308
Ilan Shan <eduilango@gmail.com>
2024-10-27 14:46:23 -0500

    changes to test the domain service

--- DomainService/src/main/java/com/example/domain/DomainController.java
+++ DomainService/src/main/java/com/example/domain/DomainController.java
@@ -2,15 +2,17 @@ package com.example.domain;
 
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController
 public class DomainController {
 
     @Autowired
     private ProducerTemplate producerTemplate;
 
-    public String callExternalMicroservice() {
+    @GetMapping("/call-external")
+    public String callExternal() {
         return producerTemplate.requestBody("direct:callMicroservice", null, String.class);
     }
 }
