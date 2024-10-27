15328f9d38c806256c19d7b5342034954794e307
Ilan Shan <eduilango@gmail.com>
2024-10-27 16:43:06 -0500

    added authentication

--- DomainService/src/main/java/com/example/domain/DomainRoute.java
+++ DomainService/src/main/java/com/example/domain/DomainRoute.java
@@ -7,8 +7,20 @@ import org.springframework.stereotype.Component;
 public class DomainRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
+        // Assuming we have a method to get the OAuth2 token
+        String token = null;
+        if (token == null) {
+            token = getOAuth2Token();
+        }
+        
         from("direct:callMicroservice")
+            //.setHeader("Authorization", simple("Bearer " + token))
             .to("http://localhost:8081/adapter/data")
             .log("Response: ${body}");
+    }
+
+    private String getOAuth2Token() {
+        // Implement the logic to get the OAuth2 token
+        return "your_oauth2_token";
     }
 }
